bfaa70bb2167bcf0e054d7d04a7ee62e3cc2115f
bfp89 <63593221+bfp89@users.noreply.github.com>
2020-10-23 16:18:34 +0100

    Add files via upload

---
+++ Gazetteer/GazetteerScript.js
@@ -0,0 +1,72 @@
+//The Map
+var map = L.map('mapid').fitWorld();
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+maxZoom: 18,
+id: 'mapbox/streets-v11',
+tileSize: 512,
+zoomOffset: -1,
+accessToken: 'pk.eyJ1IjoiYmVucDExIiwiYSI6ImNrZnM2ZGdzZTA2OHQyc2xyZjl6YWppancifQ.AeFqdkNbl4h_JnvtqBEhfw'
+}).addTo(map);
+
+
+//Navigator
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+};
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+};
+
+map.on('locationerror', onLocationError);
+
+//Button for API info 
+
+var myPopup = L.popup().setContent(
+    'Hi Hannah!'
+);
+
+L.easyButton('fa-globe', function(btn, map){
+   myPopup.setLatLng(map.getCenter()).openOn(map);
+}).addTo(map);
+
+/*L.easyButton('fa-globe fa-lg', function(btn, map){
+
+    
+
+
+}).addTo(mymap);*/
+
+//Select country - function
+    $(document).ready(function() {
+
+        $.ajax({
+            url: "libs/php/selectCountry.php",
+            type: 'POST',
+            dataType: 'json',
+            
+            success: function(result) {
+
+                console.log(result);
+            },
+            
+        });
+    });
+
+
+//Select Country - style
+$(document).ready(function() {
+    $('.js-example-basic-single').select2();
+});
+
