183af9ec43754b0350a27144558ddaf8954b8998
bfp89 <benp11@hotmail.com>
2020-12-03 14:21:12 +0000

    updated files
    updated 03/12

--- Gazetteer/libs/js/GazetteerScript.js
+++ Gazetteer/libs/js/GazetteerScript.js
@@ -31,42 +31,249 @@ function onLocationError(e) {
 
 map.on('locationerror', onLocationError);
 
-//Button for API info 
 
-var myPopup = L.popup().setContent(
-    'Hi Hannah!'
-);
 
-L.easyButton('fa-globe', function(btn, map){
-   myPopup.setLatLng(map.getCenter()).openOn(map);
-}).addTo(map);
+//Select country - function
 
-/*L.easyButton('fa-globe fa-lg', function(btn, map){
+var border;
+
+$(document).ready(function() {
 
     
 
+    $.ajax({
+        url: "libs/php/selectCountry.php",
+        type: 'POST',
+        dataType: 'json',
+        
+        success: function(result) {
 
-}).addTo(mymap);*/
+            console.log(result);
 
-//Select country - function
-    $(document).ready(function() {
+            $('#whichCountry').html('');
 
+
+
+            $.each(result.data, function(index) {
+            
+                
+            
+                $('#whichCountry').append($("<option>", {
+            
+                    value: result.data[index].code,
+            
+                    text: result.data[index].name
+            
+                })); 
+            
+            }); 
+        },
+        
+    });
+    $('#whichCountry').change(function() {
+                
         $.ajax({
-            url: "libs/php/selectCountry.php",
+            url: "libs/php/countryBorders.php",
             type: 'POST',
             dataType: 'json',
-            
-            success: function(result) {
-
-                console.log(result);
+            data: {
+                iso_a2: $('#whichCountry').val()
             },
             
+            success: function(result) {
+                console.log(result);
+
+                if (map.hasLayer(border)) {
+
+                    map.removeLayer(border);
+
+                };
+
+
+                border = L.geoJson(result.data,{
+
+                    color: '#ff7800',
+
+                    weight: 6,
+
+                    opacity: 0.65
+
+                }).addTo(map);         
+
+
+                map.fitBounds(border.getBounds());
+
+                
+                }
+
         });
+        
+        
     });
 
+//Select country - style
 
-//Select Country - style
-$(document).ready(function() {
-    $('.js-example-basic-single').select2();
+    $('.js-example-responsive').select2({
+        width: 'resolve'
+    });
+
 });
 
+//Buttons for API info 
+
+//Travel
+
+$('#travelButton').click(function() {
+
+    $.ajax({
+        url: "libs/php/gazetteer.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            alpha2Code: $('#whichCountry').val()
+        },
+
+        success: function(result) {
+
+            console.log(result);
+
+            if (result.status.name == "ok") {
+
+                $('#txtName').html(result['data']['rest']["name"]);
+                $('#txtLang').html(result['data']['rest']["languages"][0]["name"]);
+                $('#txtCurr').html(result['data']['rest']["currencies"][0]["name"]);
+                $('#txtZone').html(result['data']['rest']["timezones"]);
+                $('#txtCall').html(result['data']['rest']["callingCodes"]);
+                         
+            };
+        
+        },
+
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("info not found");
+        },
+    }); 
+
+
+});
+
+//Geography
+
+$('#geogButton').click(function() {
+
+    $.ajax({
+        url: "libs/php/gazetteer.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            alpha2Code: $('#whichCountry').val()
+        },
+
+        success: function(result) {
+
+            console.log(result);
+
+            if (result.status.name == "ok") {
+
+                $('#txtName2').html(result['data']['rest']["name"]);
+                $('#txtCapital').html(result['data']['rest']["capital"]);
+                $('#txtPop').html(result['data']['rest']["population"]);
+                $('#txtArea').html(result['data']['geonames'][0]["areaInSqKm"]);
+                $('#txtCont').html(result['data']['worldBank'][1][0]["region"]["value"]);
+                
+                
+            };
+        
+        },
+
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("info not found");
+        },
+    }); 
+
+
+});
+
+
+//Country Facts 
+
+$('#factsButton').click(function() {
+
+    $.ajax({
+        url: "libs/php/gazetteer.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            alpha2Code: $('#whichCountry').val()
+        },
+
+        success: function(result) {
+
+            console.log(result);
+
+            if (result.status.name == "ok") {
+
+                $('#txtName3').html(result['data']['rest']["name"]);
+                $('#txtIncome').html(result['data']['worldBank'][1][0]["incomeLevel"]["value"]);
+                $('#txtEu').html(result['data']['apiCast']["result"]);
+                $('#imgFlag').attr({"src": result['data']['rest']["flag"], "width": "25%", "height": "10%"});
+
+            };
+        if (result['data']['apiCast']["result"] == false) {
+
+            $('#txtEu').html("False");
+        };
+        },
+
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("info not found");
+        },
+    }); 
+
+
+});
+
+//National Holidays
+
+$('#holidayButton').click(function() {
+
+    $.ajax({
+        url: "libs/php/gazetteer.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            alpha2Code: $('#whichCountry').val(),
+            day: $('#dayDate').val(),
+            month: $('#monthDate').val(),
+            year: $('#yearDate').val()
+        },
+
+        success: function(result) {
+
+            console.log(result);
+
+            if (result['data']['holidays'][0] == null) {
+                $('#txtHol').html("No holidays on selected date");
+                $('#txtHolDate').html("No holidays on selected date");
+                $('#txtHolDay').html("No holidays on selected date");
+            };
+
+            if (result.status.name == "ok") {
+
+                $('#txtName4').html(result['data']['rest']["name"]);
+                $('#txtHol').html(result['data']['holidays'][0]["name"]);
+                $('#txtHolDate').html(result['data']['holidays'][0]["date"]);
+                $('#txtHolDay').html(result['data']['holidays'][0]["week_day"]);
+                $('#txtHolType').html(result['data']['holidays'][0]["type"]);
+
+
+            };
+        },
+
+        error: function(jqXHR, textStatus, errorThrown,) {
+            console.log("info not found");
+        },
+    }); 
+
+
+});
---
+++ Gazetteer/libs/php/gazetteer.php
@@ -0,0 +1,86 @@
+<?php
+
+	$executionStartTime = microtime(true) / 1000;
+
+
+	$url='https://restcountries.eu/rest/v2/alpha/' . $_REQUEST['alpha2Code'];
+	
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	$rest = json_decode($result,true);	
+
+	$url2='http://api.geonames.org/countryInfoJSON?formatted=true&lang=en&country=' . $_REQUEST['alpha2Code'] . '&username=benpenny1&style=full';
+
+	$ch2 = curl_init();
+	curl_setopt($ch2, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch2, CURLOPT_URL,$url2);
+
+	$result2=curl_exec($ch2);
+
+	curl_close($ch2);
+
+	$geonames = json_decode($result2,true);
+
+	$url3='http://api.worldbank.org/v2/country/' . $_REQUEST['alpha2Code'] . '?format=json';
+
+	$ch3 = curl_init();
+	curl_setopt($ch3, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch3, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch3, CURLOPT_URL,$url3);
+
+	$result3=curl_exec($ch3);
+
+	curl_close($ch3);
+
+	$worldBank = json_decode($result3,true);
+
+	$url4='https://api-2445580194301.production.gw.apicast.io/1.0/region/country/is_eu.php?country=' . $_REQUEST['alpha2Code'] . '&app_id=c11f016a&app_key=1c828fd562ef4d59c1a177e6c1d85e98';
+
+	$ch4 = curl_init();
+	curl_setopt($ch4, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch4, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch4, CURLOPT_URL,$url4);
+
+	$result4=curl_exec($ch4);
+
+	curl_close($ch4);
+
+	$apiCast = json_decode($result4,true);
+
+	$url5='https://holidays.abstractapi.com/v1/?api_key=5e0ccb7c96a3403ba76bfdc4d7c28e53&country=' . $_REQUEST['alpha2Code'] . '&year=' . $_REQUEST['year'] . '&month=' . $_REQUEST['month'] . '&day=' . $_REQUEST['day'];
+
+	$ch5 = curl_init();
+	curl_setopt($ch5, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch5, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch5, CURLOPT_URL,$url5);
+
+	$result5=curl_exec($ch5);
+
+	curl_close($ch5);
+
+	$holidays = json_decode($result5,true);
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "mission saved";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$output['data']['rest'] = $rest;
+	$output['data']['geonames'] = $geonames['geonames'];
+	$output['data']['worldBank'] = $worldBank;
+	$output['data']['apiCast'] = $apiCast;
+	$output['data']['holidays'] = $holidays;
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output); 
+
+?>
