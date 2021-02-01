5832fd2015ddf06b9a2a5547467e9ac94bcbc2f1
bfp89 <benp11@hotmail.com>
2021-02-01 14:01:26 +0000

    updates

--- companydirectory/libs/js/main.js
+++ companydirectory/libs/js/main.js
@@ -10,19 +10,15 @@ $(document).ready(function() {
             if (result.status.name == "ok") {
                 $.each(result['data'], function(index) {
                     var newCard = "<div class='card filter " + result['data'][index].department.replace(/\s+/g, '') + " " + result['data'][index].location.replace(/\s+/g, '') + " " + result['data'][index].lastName + "'><h3 class='name'>" + result['data'][index].lastName + ', <br>' + result['data'][index].firstName + "</h3><p class='dept'>" + result['data'][index].department + "</p><img src='img/employee-icon.png' alt='Profile pic' style='width:25%'><br><a id='email' href='mailto:" + result['data'][index].email + "'><i class='fa fa-envelope'></i>Email</a><br><button id='vDetails' class='btn btn-primary' data-toggle='modal' data-target='#details"+index+"'>View details</button></div>";
-                    var newDetails = "<div class='modal fade' id='details"+index+"' tabindex='-1' role='dialog'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title'>Employee Details</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'><div><table class='table table-striped table-hover'><tr><th>Name</th><td>" + result['data'][index].lastName + ", " + result['data'][index].firstName + "</td></tr><tr><th>Department</th><td>" + result['data'][index].department + "</td></tr><tr><th>Location</th><td>" + result['data'][index].location + "</td></tr><tr><th>Email</th><td>" + result['data'][index].email + "</td></tr></table></div></div><div class='modal-footer'><button type='button' class='btn btn-primary'>Save changes</button><button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button></div></div></div></div>";
-                    // var newDrop = `<a class='dropdown-item' onclick="filterSelection('${result['data'][index].lastName}')">${result['data'][index].lastName}, ${result['data'][index].firstName}</a>`;
+                    var newDetails = "<div class='modal fade' id='details"+index+"' tabindex='-1' role='dialog'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title'>Employee Details</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'><div><table class='table table-striped table-hover'><tr><th>Name</th><td>" + result['data'][index].lastName + ", " + result['data'][index].firstName + "</td></tr><tr><th>Department</th><td>" + result['data'][index].department + "</td></tr><tr><th>Location</th><td>" + result['data'][index].location + "</td></tr><tr><th>Email</th><td>" + result['data'][index].email + "</td></tr></table></div></div><div class='modal-footer'><button type='button' class='btn btn-info'>Edit Details</button><button type='button' class='btn btn-primary'>Save changes</button><button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button></div></div></div></div>";
+                    var newDrop = `<li><a onclick="filterSelection('${result['data'][index].lastName}')">${result['data'][index].lastName}, ${result['data'][index].firstName}</a></li>`;
                     
-                    $('#EmployeeName').append($("<option>", {
-                        text: result.data[index].lastName + ', ' + result['data'][index].firstName,
-                        onkeyup: "filterSelection('" + result['data'][index].lastName + "')"
-                    }));
                     $("#cards").append(newCard);
                     $("#modals").append(newDetails);    
-                    // $("#nameDrop").append(newDrop);
-                    $( "#allButton" ).trigger("click");
+                    $("#nameDrop").append(newDrop);
 
                 });
+                $("#allButton").trigger("click");
             };
         },
     });
@@ -64,3 +60,20 @@ function filterSelection(c) {
   };
   
 
+  function nameInput() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('nameInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("nameDrop");
+    li = ul.getElementsByTagName('li');
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "block";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
--- companydirectory/libs/php/config.php
+++ companydirectory/libs/php/config.php
@@ -1,9 +1,10 @@
 <?php
 
-	$cd_host = "db5001550654.hosting-data.io";
+	$cd_host = "127.0.0.1";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "dbu1221265"; // user name
-	$cd_password = "Teanotwar1!"; // password
-	$cd_dbname = "dbs1295494"; // database name
+	$cd_user = "root"; // user name
+	$cd_password = ""; // password
+	$cd_dbname = "companydirectory"; // database name
 ?>
+
